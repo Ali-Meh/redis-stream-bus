34c692af3e01e3630e38f57c51a8cd3599fd2b57
Ali Mahdavi <a5mahdavy5@gmail.com>
2021-09-24 12:20:41 +0330

    Version bump

--- src/bus.rs
+++ src/bus.rs
@@ -1,38 +1,63 @@
-use std::collections::{BTreeMap, HashMap};
-
-use super::error::Result;
+use async_trait::async_trait;
+use futures::channel::mpsc::Sender;
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc::Receiver;
+use std::collections::{BTreeMap, HashMap};
 
 #[cfg(test)]
 use mockall::*;
 
 pub type StreamID = String;
-pub type StreamKey = String;
 
 ///
-/// EventValue is the data structure passed to event handler
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// StreamValue is the data structure passed to event handler
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct StreamValue {
     /// identifier of the sender module
     pub module: String,
-    // correlation id for the request message
-    pub request_id: Option<String>,
-    // request body
+    // TODO: Do we really need it?
+    /// correlation id for the request message
+    pub request_id: Option<StreamID>,
+    /// request body
     pub message: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+///
+/// Stream is the data structure to keep streams nice and tidy
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Stream {
+    /// redis streams Id auto added via redis node
     pub id: Option<StreamID>,
-    pub key: StreamKey,
+    /// redis stream this streams belongs to
+    pub key: String,
+    /// data embedded in the event
     pub value: StreamValue,
 }
+
+impl Stream {
+    pub fn new<S: Serialize>(
+        key: &str,
+        module: &str,
+        request_id: Option<StreamID>,
+        message: S,
+    ) -> Self {
+        Stream {
+            id: None,
+            key: key.to_owned(),
+            value: StreamValue {
+                module: module.to_string(),
+                request_id,
+                message: serde_json::json!(message).to_string(),
+            },
+        }
+    }
+}
+
 #[cfg_attr(test, automock)]
-pub trait StreamBus: Sync + Send + 'static {
-    fn ack(&mut self, stream: &Stream) -> Result<()>;
-    fn add(&mut self, stream: &Stream) -> Result<StreamID>;
-    fn read(&mut self, keys: &Vec<String>) -> Result<Receiver<Stream>>;
+#[async_trait]
+pub trait StreamBus: Sync + Send {
+    fn xadd_sender(&self) -> Sender<Stream>;
+    fn xack_sender(&self) -> Sender<Stream>;
+    async fn run<'a>(mut self, keys: &[&'a str], mut read_tx: Sender<Stream>);
 }
 
 impl From<StreamValue> for BTreeMap<String, String> {
@@ -45,37 +70,28 @@ impl From<StreamValue> for BTreeMap<String, String> {
         if let Some(rid) = event.request_id {
             map.insert("request_id".to_string(), rid);
         }
-
         map
     }
 }
 
 impl From<HashMap<String, redis::Value>> for StreamValue {
     fn from(map: HashMap<String, redis::Value>) -> Self {
-        let request_id = parse_to_string(map.get("request_id"));
-        let request_id = if "".to_string() == request_id {
-            None
-        } else {
-            Some(request_id)
-        };
-
         StreamValue {
-            module: parse_to_string(map.get("module")),
-            request_id,
-            message: parse_to_string(map.get("message")),
+            module: parse_to_string(map.get("module")).unwrap_or_default(),
+            request_id: parse_to_string(map.get("request_id")),
+            message: parse_to_string(map.get("message")).unwrap_or_default(),
         }
     }
 }
 
-pub fn parse_to_string(from: Option<&redis::Value>) -> String {
+pub fn parse_to_string(from: Option<&redis::Value>) -> Option<String> {
     if let Some(v) = from {
         match v {
-            redis::Value::Data(c) => String::from_utf8(c.clone()).unwrap(),
-            redis::Value::Status(c) => c.into(),
-            // Value::Int(c)=>c.into(),
-            _ => String::from(""),
+            redis::Value::Data(c) => Some(String::from_utf8(c.clone()).unwrap()),
+            redis::Value::Status(c) => Some(c.into()),
+            _ => None,
         }
     } else {
-        String::from("")
+        None
     }
 }
--- src/client_test.rs
+++ src/client_test.rs
@@ -1,58 +1,177 @@
 #[cfg(test)]
 mod tests {
-    use crate::server::RedisServer;
-    use crate::{bus::*, client::*};
-    use simple_logger::SimpleLogger;
+    use crate::bus;
+    use crate::bus::StreamBus;
+    use crate::client::RedisClient;
+    use futures::channel::mpsc::channel;
+    use futures::select;
+    use futures::{SinkExt, StreamExt};
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use tokio::task::{self};
+
+    const REDIS_CON: &str = "redis://localhost:6379";
+
+    #[test]
+    fn test_new_stream() {
+        let stream = bus::Stream::new("key", "module", None, "message");
+
+        assert_eq!(stream.key, "key");
+        assert_eq!(stream.value.request_id, None);
+        assert_eq!(stream.value.module, "module");
+        assert_eq!(stream.value.message, "\"message\"");
+    }
 
     #[tokio::test]
-    async fn test_read_and_add() {
-        SimpleLogger::new().init().unwrap();
+    async fn test_add_stream_with_id() {
+        let client = RedisClient::new(REDIS_CON, "group_1", "consumer_1").unwrap();
 
-        // let server = RedisServer::new("127.0.0.1".to_owned(), "6379".to_owned());
-        let mut client = RedisClient::new("redis://localhost:6379")
-            .unwrap()
-            .with_consumer_name("consumer_1")
-            .with_group_name("group_1");
+        let mut add_tx = client.xadd_sender();
+        let (read_tx, mut read_rx) = channel(100);
 
+        task::spawn(async move {
+            client.run(&["key_read_id"], read_tx).await;
+        });
 
+        let mut stream_out = bus::Stream::new("key_read_id", "module_1", None, "message_1");
 
-        let mut read_rx = client
-            .read(&vec!["key_1".to_string(), "key_2".to_string()])
-            .unwrap();
+        let start = SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
 
-        let msg1 = Stream {
-            id: None,
-            key: "key_1".to_owned(),
-            value: StreamValue {
-                module: "module_1".to_owned(),
-                request_id: None,
-                message: "message_1".to_owned(),
-            },
-        };
+        stream_out.id = Some(format!("{}-0", since_the_epoch.as_millis()));
 
-        let mut msg2 = msg1.clone();
+        add_tx.send(stream_out.clone()).await.unwrap();
 
-        msg2.key = "key_2".to_owned();
-        msg2.value.module = "module_2".to_owned();
-        msg2.value.message = "message_2".to_owned();
+        let stream_in = read_rx.next().await.unwrap();
 
-        log::info!("mssages ready");
+        assert_eq!(stream_in.id, stream_out.id);
+    }
 
+    #[tokio::test]
+    async fn test_read_one_group() {
+        let client = RedisClient::new(REDIS_CON, "group_1", "consumer_1").unwrap();
 
-        client.add(&msg1).unwrap();
-        client.add(&msg2).unwrap();
+        let mut add_tx = client.xadd_sender();
+        let (read_tx, mut read_rx) = channel(100);
 
-        log::warn!("listining");
+        task::spawn(async move {
+            client.run(&["key_read_one_group"], read_tx).await;
+        });
 
+        let stream_1 = bus::Stream::new("key_read_one_group", "module_1", None, "message");
 
-        if let Some(stream) = read_rx.recv().await {
-            log::warn!("Received {:?}", stream)
-        }else{
-            log::warn!("---------- nothing recived")
+        add_tx.send(stream_1.clone()).await.unwrap();
+
+        let stream_in = read_rx.next().await.unwrap();
+
+        assert_eq!(stream_in.value.message, stream_1.value.message);
+        assert_eq!(stream_in.key, stream_1.key);
+    }
+
+    #[tokio::test]
+    async fn test_read_two_groups() {
+        let client_1 = RedisClient::new(REDIS_CON, "group_1", "consumer_1").unwrap();
+        let client_2 = RedisClient::new(REDIS_CON, "group_2", "consumer_1").unwrap();
+
+        let mut add_tx_1 = client_1.xadd_sender();
+        let (read_tx_1, mut read_rx_1) = channel(100);
+        let (read_tx_2, mut read_rx_2) = channel(100);
+
+        task::spawn(async move {
+            client_1.run(&["foo"], read_tx_1).await;
+        });
+
+        task::spawn(async move {
+            client_2.run(&["foo", "bar"], read_tx_2).await;
+        });
+
+        let stream_1 = bus::Stream::new("zoo", "module_1", None, "message_1");
+        let stream_2 = bus::Stream::new("foo", "module_2", None, "message_2");
+        let stream_3 = bus::Stream::new("bar", "module_3", None, "message_3");
+
+        add_tx_1.send(stream_1.clone()).await.unwrap();
+        add_tx_1.send(stream_2.clone()).await.unwrap();
+        add_tx_1.send(stream_3.clone()).await.unwrap();
+
+        let streams_in_1 = read_rx_1.next().await.unwrap();
+        let streams_in_2 = read_rx_2.next().await.unwrap();
+        let streams_in_3 = read_rx_2.next().await.unwrap();
+
+        assert_eq!(streams_in_1.key, "foo");
+        assert_eq!(streams_in_2.key, "foo");
+        assert_eq!(streams_in_3.key, "bar");
+    }
+
+    #[tokio::test]
+    async fn test_two_consumers() {
+        let consumer_1 = RedisClient::new(REDIS_CON, "group_1", "consumer_1").unwrap();
+        let consumer_2 = RedisClient::new(REDIS_CON, "group_1", "consumer_2").unwrap();
+
+        let mut add_tx_1 = consumer_1.xadd_sender();
+        let (read_tx_1, mut read_rx_1) = channel(100);
+        let (read_tx_2, mut read_rx_2) = channel(100);
+
+        task::spawn(async move {
+            consumer_1.run(&["key_2_consumers"], read_tx_1).await;
+        });
+
+        task::spawn(async move {
+            consumer_2.run(&["key_2_consumers"], read_tx_2).await;
+        });
+
+        let streams = [
+            bus::Stream::new("key_2_consumers", "module_1", None, "message_1"),
+            bus::Stream::new("key_2_consumers", "module_2", None, "message_2"),
+            bus::Stream::new("key_2_consumers", "module_3", None, "message_3"),
+            bus::Stream::new("key_2_consumers", "module_4", None, "message_4"),
+        ];
+
+        add_tx_1.send(streams[0].clone()).await.unwrap();
+        add_tx_1.send(streams[1].clone()).await.unwrap();
+        add_tx_1.send(streams[2].clone()).await.unwrap();
+        add_tx_1.send(streams[3].clone()).await.unwrap();
+
+        let mut modules = Vec::new();
+
+        for _ in 0..4 {
+            select! {
+                read_option = read_rx_1.next() => if let Some(stream) = read_option {
+                    modules.push(stream.value.module)
+                },
+                read_option = read_rx_2.next() => if let Some(stream) = read_option {
+                    modules.push(stream.value.module)
+                },
+            }
         }
 
-        if let Some(stream) = read_rx.recv().await {
-            println!("Received {:?}", stream)
-        }
+        assert!(modules.contains(&"module_1".to_owned()));
+        assert!(modules.contains(&"module_2".to_owned()));
+        assert!(modules.contains(&"module_3".to_owned()));
+        assert!(modules.contains(&"module_4".to_owned()));
+    }
+
+    #[tokio::test]
+    async fn test_ack() {
+        let client_1 = RedisClient::new(REDIS_CON, "group_1", "consumer_1").unwrap();
+
+        let mut add_tx = client_1.xadd_sender();
+        let mut ack_tx = client_1.xack_sender();
+        let (read_tx_1, mut read_rx_1) = channel(100);
+
+        task::spawn(async move {
+            client_1.run(&["key_ack"], read_tx_1).await;
+        });
+
+        let stream_1 = bus::Stream::new("key_ack", "module_1", None, "message_1");
+
+        let stream_2 = bus::Stream::new("key_ack", "module_1", None, "message_2");
+
+        add_tx.send(stream_1.clone()).await.unwrap();
+        let stream_1 = read_rx_1.next().await.unwrap();
+        ack_tx.send(stream_1.clone()).await.unwrap();
+
+        add_tx.send(stream_2.clone()).await.unwrap();
+        read_rx_1.next().await.unwrap();
     }
 }
