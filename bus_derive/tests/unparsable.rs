8f7bd65a14d50d3a07719426a0d8d2400b645764
Ali Mahdavi <a5mahdavy5@gmail.com>
2021-09-26 12:26:22 +0330

    remove unparsable test and update errors

--- bus_derive/tests/unparsable.rs
+++
@@ -1,9 +0,0 @@
-use bus_derive::*;
-
-#[derive(Debug, Clone, RedisStream)]
-#[stream_name("test")]
-pub struct TestStruct {
-    pub id: String,
-}
-
-fn main() {}
--- redis_stream_bus/src/error.rs
+++ redis_stream_bus/src/error.rs
@@ -1,21 +1,14 @@
-use redis::RedisError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Invalid AddressID")]
-    RedisError { msg: String },
+
+    #[error("Connection error")]
+    RedisError(#[from] redis::RedisError),
 
     #[error("Invalid Stream ID")]
     InvalidStreamID,
 }
 
-impl From<RedisError> for Error {
-    fn from(err: RedisError) -> Self {
-        Error::RedisError {
-            msg: format!("{}", err),
-        }
-    }
-}
 
 pub type Result<T> = std::result::Result<T, Error>;
